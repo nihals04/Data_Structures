c3e34561baece8ee56a3fc35da3b692f7f2ed339
nihals04 <94679263+nihals04@users.noreply.github.com>
2022-05-18 14:13:03 +0530

    Add files via upload

---
+++ V1.1.c
@@ -0,0 +1,67 @@
+#include<stdio.h>		
+#include<stdlib.h>
+void main()
+{
+	struct node
+	{
+		int data;
+		struct node *next;
+	};
+	struct node *head=NULL, *pos=NULL,*tail=NULL;
+	int ch, entry, count;
+	while(1)
+	{
+		printf("\n1.INSERT\n2.DISPLAY\n3.COUNT\n4.EXIT");	
+		printf("\nEnter your choice: ");
+		scanf("%d",&ch);
+	
+	switch(ch)
+	{
+		case 1:
+		{
+			printf("\nEnter data: ");
+			scanf("%d",&entry);
+			if(head==NULL)
+			{
+				head=(struct node*)malloc(sizeof(struct node));
+				head->data=entry;
+				pos=head;
+				tail=head;
+			}
+			else
+			{
+				tail->next=(struct node*)malloc(sizeof(struct node));
+				tail=tail->next;
+				tail->data=entry;
+			} break;
+		}
+		case 2:
+		{
+			pos=head;
+			printf("\nElements are: ");
+			while (pos!=NULL)
+			{
+				printf("\t %d",pos->data);
+				pos=pos->next;
+			} break;
+		}
+		case 3:
+		{
+			pos=head;
+			while (pos!=NULL)
+			{
+				count=count+1;
+				pos=pos->next;
+			} 
+			printf("\nNumber of elements: %d",count);
+			break;
+		}
+		case 4:
+		{
+			printf("\n !!!Good BYE!!!");
+			exit(0);
+		}						
+	}
+}
+}
+					
